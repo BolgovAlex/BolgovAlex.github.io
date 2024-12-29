862460d4984e8641f521dbfafbbbb62b0b791ac4
Alexandr Bolgov <perepodvipodvertpidzaka@gmail.com>
2024-12-30 02:35:20 +0300

    пе

--- app.py
+++ app.py
@@ -1,39 +1,43 @@
-from flask import Flask, render_template, request, redirect, url_for
-from flask_babel import Babel, gettext
-import os
+from flask import Flask, render_template, redirect, session, url_for
+from datetime import timedelta
 
 app = Flask(__name__)
-app.config['BABEL_DEFAULT_LOCALE'] = 'ru'
-app.config['BABEL_SUPPORTED_LOCALES'] = ['ru', 'en']
-
-babel = Babel(app)
-
-@babel.localeselector
-def get_locale():
-    lang = request.cookies.get('language')
-    if lang in app.config['BABEL_SUPPORTED_LOCALES']:
-        return lang
-    return request.accept_languages.best_match(app.config['BABEL_SUPPORTED_LOCALES'])
 
 @app.route("/")
 def main():
-    return render_template("index.html")
+    return redirect(url_for('ru_light'))
 
-@app.route("/change_language/<language>")
-def change_language(language):
-    if language not in app.config['BABEL_SUPPORTED_LOCALES']:
-        language = 'ru'
-    response = redirect(url_for('main'))
-    response.set_cookie('language', language)
-    return response
+@app.route("/en/dark")
+def en_dark():
+    session['theme'] = 'dark'
+    session['language'] = 'en'
+    return render_template("dark_en.html")
+
+@app.route("/en/light")
+def en_light():
+    session['theme'] = 'light'
+    session['language'] = 'en'
+    return render_template("light_en.html")
+
+@app.route("/ru/dark")
+def ru_dark():
+    session['theme'] = 'dark'
+    session['language'] = 'ru'
+    return render_template("dark_rus.html")
+
+@app.route("/ru/light")
+def ru_light():
+    session['theme'] = 'light'
+    session['language'] = 'ru'
+    return render_template("light_rus.html")
 
 @app.errorhandler(404)
 def render_not_found(error):
-    return gettext("Ничего не нашлось! Вот неудача, отправляйтесь на главную!"), 404
+    return "Ничего не нашлось! Вот неудача, отправляйтесь на главную!"
 
 @app.errorhandler(500)
 def render_server_error(error):
-    return gettext("Что-то не так, но мы все починим"), 500
+    return "Что-то не так, но мы все починим"
 
 if __name__ == '__main__':
-    app.run(port=5002, debug=True)
+    app.run(port=5005, debug=True)
